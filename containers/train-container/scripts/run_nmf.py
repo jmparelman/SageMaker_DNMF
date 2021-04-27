d7893325ac3e026d3d6082452903af68fc77ceba
jake <jmparelman@gmail.com>
2021-04-26 21:54:02 -0400

    modified dockerfile and k

--- containers/train-container/scripts/run_nmf.py
+++ containers/train-container/scripts/run_nmf.py
@@ -8,10 +8,6 @@ output_path = os.path.join(base_path,'output')
 hyperparameters = json.load(os.path.join(base_path,'input/data','hyperparameters.json'))
 k = hyperparameters['k']
 
-
-dict_ = joblib.load(input_path)
-
-
 def train(X,k,max_iter=300,random_state=1234):
     model = decomposition.NMF(n_components=k,init='nndsvd',max_iter=max_iter,random_state=random_state)
     W = model.fit_transform(X)
@@ -19,7 +15,7 @@ def train(X,k,max_iter=300,random_state=1234):
     return W,H,model
 
 if __name__ == "__main__":
-    parser = OptionParser(usage="usage: %prog [options] chamber k")
+    parser = OptionParser(usage="usage: %prog [options] chamber")
     parser.add_option('--miter',action='store',type='int', dest='max_iter')
     parser.add_option('--r',action='store',type='int', dest='random_state',default=1234)
     (options,args) = parser.parse_args()
