3fa5380f6ca5c756ecc611fa61c405092d57596a
jake <jmparelman@gmail.com>
2021-04-26 21:38:06 -0400

    adding run and output

--- containers/train-container/scripts/run_nmf.py
+++ containers/train-container/scripts/run_nmf.py
@@ -2,10 +2,9 @@ import joblib, os, json
 from sklearn import decomposition
 from optparse import OptionParser
 
-base_path = '/opt/ml'
-input_path = os.path.join(base_path,'input/data')
+base_path = '/opt/ml/train'
+input_path = os.path.join(base_path,'input')
 output_path = os.path.join(base_path,'output')
-model_path = os.path.join(base_path,'model')
 hyperparameters = json.load(os.path.join(base_path,'input/data','hyperparameters.json'))
 k = hyperparameters['k']
 
@@ -20,10 +19,14 @@ def train(X,k,max_iter=300,random_state=1234):
     return W,H,model
 
 if __name__ == "__main__":
-    parser = OptionParser(usage="usage: %prog [options] chamber")
-    parser.add_option('--k',action='store',type='int',dest='k')
+    parser = OptionParser(usage="usage: %prog [options] chamber k")
     parser.add_option('--miter',action='store',type='int', dest='max_iter')
     parser.add_option('--r',action='store',type='int', dest='random_state',default=1234)
     (options,args) = parser.parse_args()
-    k = args[0]
-    
+    chamber = args[0]
+
+    dtm_dict = joblib.load(os.path.join(input_path,f'{chamber}.pkl'))
+    dtm = dtm_dict['dtm']
+
+    W,H,model = train(dtm,k,options.max_iter,options.random_state)
+    joblib.dump({"W":W,"H":H,"model":model},os.path.join(output_path,f'{chamber}.pkl'))
