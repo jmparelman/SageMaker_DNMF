87a7b007e1a28f515b79c8990fa58617e0f7e695
jake <jmparelman@gmail.com>
2021-04-30 15:00:28 +0000

    nmf multiprocessing
    run_nmf.py and deployment were modified to run all 64 CPU on a m1.m4.16xlarge for k in the range 40-100

--- containers/train-container/scripts/run_nmf.py
+++ containers/train-container/scripts/run_nmf.py
@@ -1,42 +1,38 @@
 import joblib, os, json, argparse
 from sklearn import decomposition
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+import time
 
 base_path = '/opt/ml/'
 input_path = os.path.join(base_path,'input/data')
 output_path = os.path.join(base_path,'output')
 model_path = os.path.join(base_path,'model')
-with open(os.path.join(base_path,'input/config/hyperparameters.json')) as F:
-    trainingParams = json.load(F)
 
-channel = 'training'
-training_path = os.path.join(input_path,channel)
 
-def train():
-    
-    # get K and convert to int
-    k = trainingParams.get('k',None)
-    if k is not None:
-        k = int(k)
-    else:
-        k = 50
-    
-    # read in the data, should be a single .pkl file
-    training_file = os.listdir(training_path)[0]
-    input_dict = joblib.load(os.path.join(training_path,training_file))
-    X = input_dict['dtm']
-    
-    # get the chamber name
-    chamber = training_file.split('.')[0]
-    
-    # fit model
+training_path = os.path.join(input_path,'training')
+
+# read in the data, should be a single .pkl file
+training_file = os.listdir(training_path)[0]
+input_dict = joblib.load(os.path.join(training_path,training_file))
+X = input_dict['dtm']
+
+# get the chamber name
+chamber = training_file.split('.')[0]
+
+def run_model(k):
+    print(f'{k}.k starting')
+    start_time = time.time()
     model = decomposition.NMF(n_components=k,init='nndsvd',max_iter=200,random_state=1234)
     W = model.fit_transform(X)
     H = model.components_
     model_dict = {"W":W,'H':H,"model":model}
+    print(f'{k}.k finished -- {round((time.time() - start_time)/60),2}m')
     
-    print("coherence: 1")
     with open(os.path.join(model_path,f'NMF_{chamber}_{k}.pkl'),'wb') as out:
-        joblib.dump(model_dict,out)
+        joblib.dump(model_dict,out)  
 
 if __name__ == "__main__":
-    train()
+    # Uses m1.m4.16xlarge instance for multiprocessing (k = 40-100)
+    with Pool(64) as p:
+        p.map(run_model,range(40,101))
