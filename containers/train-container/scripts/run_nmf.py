490f2908d1f484d39cd9a7897e083e8344511221
jake <jmparelman@gmail.com>
2021-05-03 13:21:11 -0400

    added tc-w2v coherence

--- containers/train-container/scripts/run_nmf.py
+++ containers/train-container/scripts/run_nmf.py
@@ -16,9 +16,12 @@ training_path = os.path.join(input_path,'training')
 training_file = os.listdir(training_path)[0]
 input_dict = joblib.load(os.path.join(training_path,training_file))
 X = input_dict['dtm']
+terms = input_dict['vocab']
+speech_id = input_dict['speech_id']
 
 # get the chamber name
 chamber = training_file.split('.')[0]
+
 
 def run_model(k):
     print(f'{k}.k starting')
@@ -26,7 +29,7 @@ def run_model(k):
     model = decomposition.NMF(n_components=k,init='nndsvd',max_iter=200,random_state=1234)
     W = model.fit_transform(X)
     H = model.components_
-    model_dict = {"W":W,'H':H,"model":model}
+    model_dict = {"W":W,'H':H,"model":model,'terms':terms,'speech_id':speech_id}
     print(f'{k}.k finished -- {round((time.time() - start_time)/60),2}m')
     
     with open(os.path.join(model_path,f'NMF_{chamber}_{k}.pkl'),'wb') as out:
